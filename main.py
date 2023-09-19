2fe85d57fde97fc94cd5d09c010dc05e9bf675f6
unm1719223777 <144248180+unm1719223777@users.noreply.github.com>
2023-09-19 13:30:09 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-#factorial of given number
-def factorial(n):
-  if(n==0 or n==1):
-    return 1
-  else:
-    return n * factorial(n -1)
-num = 5
-r=factorial(num)
-print("factorial of {} is {} .". format(num,r))
+class Player:
+  def Play(self):
+    print("The player is playing cricket.")
+class Batsman(Player):
+  def play(self):
+    print("The player is batting.")
+class Bowler(Player):    
+  def play(self):
+    print("The bowler is bowling.")
+batsman= Batsman()
+bowler=Bowler()
+batsman.play()
+bowler.play()
